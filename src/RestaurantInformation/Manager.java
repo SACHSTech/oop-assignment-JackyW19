3fd1ec90895aa259d902006a345cb9178e8a601c
JackyW19 <jacky.wang23@ycdsbk12.ca>
2022-12-21 10:38:40 -0500

    completed manager file

--- src/RestaurantInformation/Manager.java
+++ src/RestaurantInformation/Manager.java
@@ -1,4 +1,47 @@
 package RestaurantInformation;
-public class Manager {
-    
+
+public class Manager extends Employee{
+    // Instance variable
+  private String currentProject;
+
+  /**
+    * Constructor - creates a new Manager instance
+    * @param fname - the Manager's first name
+    * @param lname - the Manager's last name
+    * @param address - the Manager's home address
+    * @param med - the Manager's medical information
+    * @param sal - the Manager's salary
+    * @param project - the Manager's project
+    */
+  public Manager(String fname, String lname, String address, String med, int sal, String project){
+    // Triggers a parent constructor in Employee class
+    super(fname, lname, address, med, sal);
+    currentProject = project;
+  }
+
+  /**
+  * Getter method for manager's current project
+  * @return manager's current project
+  */
+  public String getCurrentProject(){
+    return currentProject;
+  }
+
+  /**
+  * String representation of a Manager
+  * @return all the attributes regarding a Manager
+  */
+  public String toString(){
+    System.out.println("--------------------------------------");
+    System.out.println("Position: Software Development Manager");
+    System.out.println("First Name: " + getFirstName());
+    System.out.println("Last Name: " + getLastName());
+    System.out.println("Home Address: " + getAddress());
+    System.out.println("Medical Information: " + getMedical());
+    System.out.println("Salary: $" + getSalary());
+    System.out.println("Current Project: " + currentProject);
+    return "--------------------------------------";
+  }
+
 }
+
